3da5ac7f2223608024e4adbe0e1af55615199426
Nick-v1 <nikos.voulgaridis1@gmail.com>
2024-11-06 14:08:10 +0200

    owner repository and implementations

---
+++ Technico.Main/Repositories/Implementations/OwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Technico.Main.Data;
+
+namespace Technico.Main.Repositories.Implementations
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly TechnicoDbContext _context;
+
+        public OwnerRepository(TechnicoDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
