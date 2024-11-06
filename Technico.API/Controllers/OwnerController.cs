7c72c67603901daff9fafbb21177c3eb27cae154
Nick-v1 <nikos.voulgaridis1@gmail.com>
2024-11-06 12:50:18 +0200

    added main project

---
+++ Technico.API/Controllers/OwnerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Technico.API.Controllers;
+
+public class OwnerController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
