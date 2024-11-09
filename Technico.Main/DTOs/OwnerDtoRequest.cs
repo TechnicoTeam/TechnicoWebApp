5ad1b6fb2d8cb4d1aed64b3b9c9da80a3d3931bb
vmalefioudakis <benmalef@hotmail.com>
2024-11-09 10:34:13 +0200

    Create the owner DTOs
    -- Create a  Response Dto
    -- Create a Request Dto

---
+++ Technico.Main/DTOs/OwnerDtoRequest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Technico.Main.Models.Enums;
+
+namespace Technico.Main.DTOs
+{
+    public class OwnerDtoRequest
+    {
+        public Guid Id { get; set; }
+        required public string Vat { get; set; }
+        required public string Firstname { get; set; }
+        required public string Lastname { get; set; }
+        required public string Email { get; set; }
+        required public string Password { get; set; }
+
+        required public string Phone { get; set; }
+
+        public string Address { get; set; } // the user is not required to fill this 
+        public TypeOfUser Role { get; set; }
+        public List<Property>? Properties { get; set; }
+    }
+}
