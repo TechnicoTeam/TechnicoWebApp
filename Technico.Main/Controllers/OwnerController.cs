bc1e3f15724575756e228865335bb8b9e920564d
Nick-v1 <nikos.voulgaridis1@gmail.com>
2024-11-11 22:19:31 +0200

    added adminview in owner controller

--- Technico.Main/Controllers/OwnerController.cs
+++ Technico.Main/Controllers/OwnerController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Technico.Main.Models.Enums;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Technico.Main.Controllers
@@ -54,6 +55,52 @@ namespace Technico.Main.Controllers
             }
 
             return Json(new { success = false, message = "Failed to save profile." });
+        }
+
+        private List<Owner> _owners = new List<Owner>
+        {
+            new Owner
+            {
+                Id = Guid.NewGuid(),
+                Vat = "123456789",
+                Firstname = "John",
+                Lastname = "Doe",
+                Email = "john.doe@example.com",
+                Password = "mypassword123",
+                Phone = "1234567890",
+                Address = "123 Main St, Anytown USA",
+                Role = TypeOfUser.Admin
+            },
+            new Owner
+            {
+                Id = Guid.NewGuid(),
+                Vat = "987654321",
+                Firstname = "Jane",
+                Lastname = "Smith",
+                Email = "jane.smith@example.com",
+                Password = "mypassword456",
+                Phone = "0987654321",
+                Address = "456 Oak Rd, Someplace",
+                Role = TypeOfUser.User
+            },
+            new Owner
+            {
+                Id = Guid.NewGuid(),
+                Vat = "123789456",
+                Firstname = "Bob",
+                Lastname = "Johnson",
+                Email = "bob.johnson@example.com",
+                Password = "mypassword789",
+                Phone = "5555555555",
+                Address = "789 Elm St, Elsewhere",
+                Role = TypeOfUser.Undedified
+            }
+        };
+
+        [HttpGet("AdminView")]
+        public IActionResult AdminView()
+        {
+            return View(_owners);
         }
 
     }
