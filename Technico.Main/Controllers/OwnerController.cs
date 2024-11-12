8a974f02f6dc0c1382f0ef20508d905eab816ba6
Veniamin Malefioudakis <58257722+benmalef@users.noreply.github.com>
2024-11-12 11:04:40 +0200

    update the ownerAdminView

--- Technico.Main/Controllers/OwnerController.cs
+++ Technico.Main/Controllers/OwnerController.cs
@@ -34,50 +34,13 @@ namespace Technico.Main.Controllers
 
      
 
-        private List<Owner> _owners = new List<Owner>
-        {
-            new Owner
-            {
-                Id = Guid.NewGuid(),
-                Vat = "123456789",
-                Firstname = "John",
-                Lastname = "Doe",
-                Email = "john.doe@example.com",
-                Password = "mypassword123",
-                Phone = "1234567890",
-                Address = "123 Main St, Anytown USA",
-                Role = TypeOfUser.Admin
-            },
-            new Owner
-            {
-                Id = Guid.NewGuid(),
-                Vat = "987654321",
-                Firstname = "Jane",
-                Lastname = "Smith",
-                Email = "jane.smith@example.com",
-                Password = "mypassword456",
-                Phone = "0987654321",
-                Address = "456 Oak Rd, Someplace",
-                Role = TypeOfUser.User
-            },
-            new Owner
-            {
-                Id = Guid.NewGuid(),
-                Vat = "123789456",
-                Firstname = "Bob",
-                Lastname = "Johnson",
-                Email = "bob.johnson@example.com",
-                Password = "mypassword789",
-                Phone = "5555555555",
-                Address = "789 Elm St, Elsewhere",
-                Role = TypeOfUser.Undedified
-            }
-        };
+   
 
         [HttpGet("AdminView")]
-        public IActionResult AdminView()
+        public async Task<IActionResult> AdminView()
         {
-            return View(_owners);
+            var owners = await _ownerService.GetAllOwners();
+            return View(owners.ToList());
         }
 
     }
